c0185abb541dfa29fa7aeb5a7d8ef70b4c333c20
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-18 13:53:51 +0300

    Create PersonViewSet

--- person/views.py
+++ person/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from person.models import Person
+from person.serializers import PersonSerializer
 
-# Create your views here.
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
