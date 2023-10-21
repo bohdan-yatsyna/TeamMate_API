13cd6c79d54a1a536742e558d7fe052953853e1a
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 15:31:08 +0300

    Add documentation details for all views in person/views.py

--- person/views.py
+++ person/views.py
@@ -1,4 +1,8 @@
 from django.contrib.auth import get_user_model
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny, IsAdminUser
 
@@ -6,10 +10,29 @@ from person.serializers import CreatePersonSerializer, ManagePersonSerializer
 
 
 class RegisterPersonView(generics.CreateAPIView):
+    """
+    View for Person registering on the portal, Authentication is not required.
+    """
     serializer_class = CreatePersonSerializer
     permission_classes = (AllowAny,)
 
 
+# Only for documentation endpoint details
+@extend_schema_view(
+    get=extend_schema(
+        description="Endpoint with detailed Person page for current user."
+    ),
+    put=extend_schema(
+        description="Endpoint for updating current Person details by id."
+    ),
+    patch=extend_schema(
+        description="Endpoint for partial updating "
+                    "current Person details by id."
+    ),
+    delete=extend_schema(
+        description="Endpoint for deleting current Person's account by id."
+    )
+)
 class ManagePersonView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ManagePersonSerializer
 
@@ -17,6 +40,38 @@ class ManagePersonView(generics.RetrieveUpdateDestroyAPIView):
         return self.request.user
 
 
+# Only for documentation endpoint details
+@extend_schema_view(
+    create=extend_schema(
+        description=(
+            "Endpoint for creating Person by Admin user."
+        )
+    ),
+    retrieve=extend_schema(
+        description=(
+            "Endpoint with detailed Person's page by id for Admin users."
+            )
+        ),
+    update=extend_schema(
+        description=(
+            "Endpoint for updating a specific "
+            "Person's details by id for Admin users."
+        )
+    ),
+    partial_update=extend_schema(
+        description=(
+            "Endpoint for partially updating a specific "
+            "Person's details by id for Admin users."
+        )
+    ),
+    destroy=extend_schema(
+        description="Endpoint for deleting a specific "
+                    "Person by id for Admin users."
+    ),
+    list=extend_schema(
+        description="Endpoint for listing all Persons for Admin users."
+    ),
+)
 class AdminPersonViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = ManagePersonSerializer
