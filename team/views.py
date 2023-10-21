021431b6caaa625fb7230a94dad4a1c8916aa81d
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 14:23:37 +0300

    Update TeamViewSet with a new permissions & access logic

--- team/views.py
+++ team/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
 from team.models import Team
 from team.serializers import TeamSerializer
 
@@ -6,3 +8,20 @@ from team.serializers import TeamSerializer
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+    permission_classes_by_action = {
+        "default": (IsAdminUser,),
+        "list": (IsAuthenticated,),
+        "retrieve": (IsAuthenticated,),
+    }
+
+    def get_permissions(self):
+        try:
+            return [
+                permission()
+                for permission in self.permission_classes_by_action[self.action]
+            ]
+        except KeyError:
+            return [
+                permission()
+                for permission in self.permission_classes_by_action["default"]
+            ]
