b1a274b18cc56008b5342433f99ee1c53d0f3d5b
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 15:31:23 +0300

    Add documentation details TeamViewSet

--- team/views.py
+++ team/views.py
@@ -1,3 +1,7 @@
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+)
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
@@ -5,6 +9,38 @@ from team.models import Team
 from team.serializers import TeamSerializer
 
 
+# Only for documentation endpoint details
+@extend_schema_view(
+    create=extend_schema(
+        description=(
+            "Endpoint for creating Team by Admin user."
+        )
+    ),
+    retrieve=extend_schema(
+        description=(
+            "Endpoint with detailed Team's page by id "
+            "for Authenticated users."
+            )
+        ),
+    update=extend_schema(
+        description=(
+            "Endpoint for updating a specific Team details by id for Admin users."
+        )
+    ),
+    partial_update=extend_schema(
+        description=(
+            "Endpoint for partially updating a specific Team details "
+            "by id for Admin users."
+        )
+    ),
+    destroy=extend_schema(
+        description="Endpoint for deleting a specific"
+                    " Team by id for Admin users."
+    ),
+    list=extend_schema(
+        description="Endpoint for listing all Teams for Authenticated users."
+    ),
+)
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
