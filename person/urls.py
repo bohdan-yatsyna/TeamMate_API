be33d9dda0eedc23d850a8b3e842102cce30cdcd
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 14:21:09 +0300

    Refactor totally person app urls accordingly to new logic of access & permissions

--- person/urls.py
+++ person/urls.py
@@ -1,9 +1,30 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from person.views import PersonViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from person.views import (
+    AdminPersonViewSet,
+    ManagePersonView,
+    RegisterPersonView,
+)
+
 
 app_name = "persons"
 
 router = DefaultRouter()
-router.register("", PersonViewSet, basename="person")
+router.register("", AdminPersonViewSet)
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path("register/", RegisterPersonView.as_view(), name="register"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair_and_login",
+    ),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("admin_user/", include(router.urls), name="admin_only"),
+    path("me/", ManagePersonView.as_view(), name="manage"),
+]
