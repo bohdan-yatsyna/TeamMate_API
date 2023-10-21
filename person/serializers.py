a0493b8e7bdf3441cf4fade8820a9720052eafa3
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-21 14:19:18 +0300

    Add CreatePersonSerializer & ManagePersonSerializer instead of PersonSerializer

--- person/serializers.py
+++ person/serializers.py
@@ -2,7 +2,23 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class CreatePersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("email", "password", "first_name", "last_name", "team")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 8,
+            }
+        }
+
+    def create(self, validated_data):
+        """Create a new Person(user) with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class ManagePersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = (
@@ -11,22 +27,24 @@ class PersonSerializer(serializers.ModelSerializer):
             "password",
             "first_name",
             "last_name",
-            "is_staff"
+            "is_staff",
+            "team"
         )
-        read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
-
-    def create(self, validated_data):
-        """Create a new Person(user) with encrypted password and return it"""
-        return get_user_model().objects.create_user(**validated_data)
+        read_only_fields = ("is_staff", "email", "id")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 8,
+                "required": False
+            }
+        }
 
     def update(self, instance, validated_data):
-        """Update a Person(user), set the password correctly and return it"""
         password = validated_data.pop("password", None)
-        user = super().update(instance, validated_data)
+        instance = super().update(instance, validated_data)
 
         if password:
-            user.set_password(password)
-            user.save()
+            instance.set_password(password)
+            instance.save()
 
-        return user
+        return instance
