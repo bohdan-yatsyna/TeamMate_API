78228647aa3570638aa509e10093ed33556c3eab
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-10-22 10:19:09 +0300

    Fix Flake8 issue in settings.py

--- teammate_app/settings.py
+++ teammate_app/settings.py
@@ -148,7 +148,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination."
+                                "LimitOffsetPagination",
     "PAGE_SIZE": 10,
 }
 
